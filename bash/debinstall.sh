b1107d25ece07cebb31f9b72c4ed7d8e020e0883
Matteo Cafasso <noxdafox@gmail.com>
2016-06-13 21:48:12 +0300

    Added debinstall script
    Script for installing Debian packages from a local folder.
    
    Signed-off-by: Matteo Cafasso <noxdafox@gmail.com>

---
+++ bash/debinstall.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+# Set up a temporary debian mirror allowing it to install packages
+# from a local folder.
+
+# Usage: debinstall.sh <folder> <deb-package> <deb-package>
+
+FOLDER=$1
+SRCLIST=/etc/apt/sources.list.d/debinstall_tmp.list
+
+set -e
+
+# ensure the folder exists
+if [ ! -d $FOLDER ]; then
+    echo "$FOLDER does not exists"
+    exit 1
+fi
+
+# set up local mirror in folder
+dpkg-scanpackages $FOLDER | gzip > $FOLDER/Packages.gz
+
+# add mirror to sources list
+cat <<EOF > $SRCLIST
+deb [trusted=yes] file:///$(dirname ${FOLDER#/}) $(basename $FOLDER)/
+EOF
+
+# update package collection
+apt-get update
+
+# install requested files
+shift
+apt-get install -y $@
+
+# remove mirror from sources list
+rm $SRCLIST
+
+# update package collection
+apt-get update
+
+exit 0
