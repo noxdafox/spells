b5842eb07460c688a31459e1fae0867e75a2854b
Matteo Cafasso <noxdafox@gmail.com>
2017-11-12 22:07:17 +0200

    linter: use .gitignore entries to filter candidates for linters
    It turns out PyLint interface is quite poor and does not allow to
    exclude properly the files.
    
    The new logic reads the .gitignore file and uses it to filter the
    candidates based on its content.
    
    Signed-off-by: Matteo Cafasso <noxdafox@gmail.com>

--- guile/linter.scm
+++ guile/linter.scm
@@ -6,6 +6,11 @@
 ;; INFO: supported programming languages (Language: linters):
 ;; INFO:  - Python: pep8, pylint
 
+(add-to-load-path "/home/noxdafox/development/spells/guile")
+
+(load "helpers.scm")
+(use-modules (helpers))
+
 (use-modules (ice-9 ftw))
 (use-modules (ice-9 match))
 (use-modules (ice-9 popen))
@@ -16,10 +21,8 @@
 (define python-file-regex ".*.py$")
 (define gitignore-file ".gitignore")
 
-(define pep8-checker
-  "pep8 --max-line-length=80 --exclude=~s ~a")
-(define pylint-checker
-  "pylint3 --max-line-length=80 --ignore=~s ~a 2> /dev/null")
+(define pep8-checker "pep8 --max-line-length=80 ~a")
+(define pylint-checker "pylint3 --max-line-length=80 ~a 2> /dev/null")
 
 (define min-pylint-score 8)
 (define pylint-score-regex "1{0,1}[0-9].[0-9]")
@@ -39,26 +42,27 @@
 ")
 
 (define (main args)
-  (let ((python-files (find-python-files "."))
-        (git-exclusions (gitignore-files gitignore-file)))
+  (let* ((git-exclusions (gitignore-files gitignore-file))
+         (python-files (find-python-files "." git-exclusions)))
     ; Python project validation
     (when (not (null? python-files))
-          (if (run-pep8-check python-files git-exclusions)
-              (if (run-pylint-check python-files git-exclusions)
-                  (exit 0)))
-          (exit 1))))
+          (let ((pep8-status (run-pep8-check python-files))
+                (pylint-status (run-pylint-check python-files)))
+            (if (and pep8-status pylint-status)
+                (exit 0)
+                (exit 1))))))
 
-(define (run-pep8-check files exclusions)
+(define (run-pep8-check files)
   ;; Check the list of Python files with Pep8
-  (let* ((results (run-checker pep8-checker files exclusions))
+  (let* ((results (run-checker pep8-checker files))
          (check-status (cond ((eq? (cdr results) 0) #t)
                              (else (display (car results)) #f))))
     (format #t pep8-message (if check-status "SUCCESS" "FAILURE"))
     check-status))
 
-(define (run-pylint-check files exclusions)
+(define (run-pylint-check files)
   ;; Check the list of Python files with PyLint
-  (let* ((results (run-checker pylint-checker files exclusions))
+  (let* ((results (run-checker pylint-checker files))
          (score (pylint-score (car results)))
          (check-status (cond ((or (eq? score 0) (> score min-pylint-score)) #t)
                              (else (display (car results)) #f))))
@@ -77,11 +81,10 @@
               0))
         0)))
 
-(define (run-checker checker-command files exclusions)
+(define (run-checker checker-command files)
   ;; Run the given checker command string against the given list of files
   ;; Returns a pair containing the command output and its exit status
-  (let* ((command (format #f checker-command
-                          (string-join exclusions ",") (string-join files)))
+  (let* ((command (format #f checker-command (string-join files)))
          (port (open-input-pipe command)))
     (cons (get-string-all port) (status:exit-val (close-pipe port)))))
 
@@ -98,10 +101,13 @@
                 (string-split text #\newline)))
       '()))
 
-(define (find-python-files root)
+(define (find-python-files root exclusions)
   ;; Search all Python files in the folder given as root
+  ;; Paths matching the patterns in the exclusions will be filtered away
   (filter (lambda (path)
-            (string-match python-file-regex path))
+            (and (string-match python-file-regex path)
+                 (not (any (lambda (exclusion)
+                             (fnmatch exclusion path 0)) exclusions))))
           (traverse-filesystem (file-system-tree root))))
 
 (define traverse-filesystem
