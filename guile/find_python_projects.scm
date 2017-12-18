13ebc1a21e9f0ffdd749a1bb196004dc868ee623
Matteo Cafasso <noxdafox@gmail.com>
2017-12-18 23:01:50 +0200

    find_python_projects: refactor search logic
    Use `file-system-fold` to better control the directory crawling.
    
    Signed-off-by: Matteo Cafasso <noxdafox@gmail.com>

--- guile/find_python_projects.scm
+++ guile/find_python_projects.scm
@@ -5,55 +5,46 @@
 ;; INFO: list all Python projects at the given path
 ;; INFO: output in CSV form with ":" as a separator
 ;; INFO: Python projects are those with a setup.py file in their root folder
+;;
+;; INFO: Usage: <script> <folder> <max-traversing-depth>
 
 (use-modules (ice-9 ftw))
 (use-modules (ice-9 match))
 (use-modules (ice-9 regex))
 (use-modules (srfi srfi-1))
 
-(define max-depth 4)
 (define dirname-regex (make-regexp ".*/"))
 (define python-project-regex (make-regexp ".*/setup.py"))
 
 (define (main args)
-  (let ((path (string-trim-right (last args) #\/)))
+  (let ((path (string-trim-right (second args) #\/))
+        (max-depth (string->number (last args))))
     (display (string-join (find-python-projects path max-depth) ":"))))
 
 (define (find-python-projects root max-depth)
-  ;; Search all Python projects in the folder given as root
-  (map (lambda (path)
-         ; remove basename of both root and path
-         (string-join (list (dirname root) (dirname path)) ""))
-       (filter (lambda (path)
-                   (regexp-exec python-project-regex path))
-               (traverse-filesystem root max-depth))))
+  (let ((current-depth 0))
+    (define (enter? path stat result)
+      (< current-depth max-depth))
 
-(define (traverse-filesystem root max-depth)
-  ;; Traverse the filesystem from root up until max-depth is reached
-  ;; return a flat list with the content of given folder
-  (let ((depth 0))
-    (filesystem-paths
-     (file-system-tree
-      root
-      (lambda (name stat)
-        (cond ((< depth max-depth) (set! depth (+ depth 1)) #t)
-              (else (set! depth (- depth 1))#f)))))))
+    (define (leaf path stat result)
+      (if (regexp-exec python-project-regex path)
+          (cons (dirname path) result)
+          result))
 
-(define filesystem-paths
-  ;; Discard the stat information retrieved by file-system-tree and join
-  ;; the path of the files. Folders are discarded.
-  (match-lambda
-   ((name stat) name)
-   ((name stat children ...)
-    (join-path name (map filesystem-paths children)))))
+    (define (down path stat result)
+      (set! current-depth (+ current-depth 1))
+      result)
+    (define (up path stat result)
+      (set! current-depth (- current-depth 1))
+      result)
 
-(define (join-path parent children)
-  (cond ((null? children) '())
-        ((pair? (car children))
-         (append (join-path parent (car children))
-                 (join-path parent (cdr children))))
-        (else (cons (string-join (list parent (car children)) "/")
-                    (join-path parent (cdr children))))))
+    (define (skip path stat result) result)
+
+    (define (error path stat errno result)
+      (format (current-error-port) "warning: ~a: ~a~%" path (strerror errno))
+      result)
+
+    (file-system-fold enter? leaf down up skip error '() root)))
 
 (define (dirname path)
   (match:substring (regexp-exec dirname-regex path)))
